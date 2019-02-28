7feacaed0c32ed18c908db1b5eb5a4ae131fd9e4
Elizaaaaa <elizazhang087@gmail.com>
2019-02-28 15:29:07 -0800

    bvh will not have repeating frames

--- hmr/3dpose_estimate.sh
+++ hmr/3dpose_estimate.sh
@@ -5,7 +5,7 @@ for f in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
 	filename=$(basename -- "$f")
   no_ext="${filename%.*}"
   
-  #echo "Processing $no_ext"
+  echo "Processing $no_ext"
   
   python2 hmr/demo.py --img_path $f \
                      --json_path keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/$no_ext.json  
