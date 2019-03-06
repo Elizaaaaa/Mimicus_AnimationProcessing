938d6fb58471f21b38dd7ffab8e4d636914ab622
Elizaaaaa <elizazhang087@gmail.com>
2019-03-05 17:44:54 -0800

    adjust the bash script

--- hmr/3dpose_estimate.sh
+++ hmr/3dpose_estimate.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-for f in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
+#for f in keras_Realtime_Multi-Person_Pose_Estimation/sample_images/*; do
 
-	filename=$(basename -- "$f")
-  no_ext="${filename%.*}"
+#	filename=$(basename -- "$f")
+#  no_ext="${filename%.*}"
   
-  echo "Processing $no_ext"
+#  echo "Processing $no_ext"
   
-  python2 hmr/demo.py --img_path $f \
-                     --json_path keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/$no_ext.json
-  
-done
-
-cd hmr
-python classify_images.py
-python join_csv.py
-cd ..
+  python2 hmr/demo.py --img_path keras_Realtime_Multi-Person_Pose_Estimation/sample_images/ \
+                     --json_path keras_Realtime_Multi-Person_Pose_Estimation/sample_jsons/
 
 echo "Done"
